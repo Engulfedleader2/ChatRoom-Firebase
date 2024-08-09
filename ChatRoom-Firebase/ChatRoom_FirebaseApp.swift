c65f3eceabba18dee0479d1484ab5b22262318ca
Engulfedleader2 <engulfedleader@gmail.com>
2024-08-08 21:30:18 -0400

    Initial Commit

---
+++ ChatRoom-Firebase/ChatRoom_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatRoom_FirebaseApp.swift
+//  ChatRoom-Firebase
+//
+//  Created by Israel on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatRoom_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
